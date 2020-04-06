689a0fff123f79025956d23f4ce5b685911f5446
TJWang <tengjingwnag@gmail.com>
2020-04-06 18:40:17 +0800

    fix #1: rds security group issues

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -10,6 +10,7 @@ resource "aws_db_instance" "mysql-rdb" {
     password             = "${var.mysql_admin_password}"
     parameter_group_name = "default.mysql5.7"
     db_subnet_group_name = "${aws_db_subnet_group.private.name}"
+    vpc_security_group_ids = ["${aws_security_group.rds.id}"]
 
     tags = {
         Name = "bionime_assi"
@@ -26,4 +27,28 @@ resource "aws_db_subnet_group" "private" {
     tags = {
         Name = "bionime_assi"
     }
+}
+
+resource "aws_security_group" "rds" {
+  name        = "rds"
+  description = "Allow MySQL inbound traffic"
+  vpc_id      = "${aws_vpc.bionime_assi.id}"
+
+  ingress {
+    description = "alow mysql connection in VPC"
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "TCP"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "bionime-assi"
+  }
 }
